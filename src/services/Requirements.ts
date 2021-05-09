391b76d0210f6f22372cef3d5a66816650c2131a
ArthurDelamare <arthur.delamare1@viacesi.fr>
2021-05-09 14:22:03 +0200

    feat: check pyenv requirement

--- src/services/Requirements.ts
+++ src/services/Requirements.ts
@@ -9,3 +9,13 @@ export async function checkPython (): Promise<boolean> {
     return false
   }
 }
+
+export async function checkPyenv (): Promise<boolean> {
+  try {
+    const { stdout } = await execa('pyenv --v')
+    const version = stdout.search('(d+.)?(d+.)?(d+)')
+    return version !== undefined
+  } catch {
+    return false
+  }
+}
