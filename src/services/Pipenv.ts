fa1cb803695df4e379296da7cad38236e6bd180e
ArthurDelamare <arthur.delamare1@viacesi.fr>
2021-06-13 16:17:03 +0200

    feat(pipenv): set pipenv path function

--- src/services/Pipenv.ts
+++ src/services/Pipenv.ts
@@ -2,6 +2,8 @@ import execa from 'execa'
 import ora from 'ora'
 import { log } from './Log'
 
+import { checkEnvironmentVariable } from './Requirements'
+
 export async function installPipenv() {
   const pipenvLoader = ora('Installing pipenv').start()
   try {
@@ -15,4 +17,31 @@ export async function installPipenv() {
       value: error.toString()
     })
   }
+}
+
+export async function setPipenvPath() {
+  const appdataDir = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share")
+  const sitePackageEnv = `${appdataDir}\\Python\\Python39\\site-packages`
+  const scriptEnv = `${appdataDir}\\Python\\Python39\\Scripts`
+
+  try {
+    if (!await checkEnvironmentVariable('PATH', sitePackageEnv)) {
+      if (process.platform === "win32") {
+        await execa(`[Environment]::SetEnvironmentVariable('PATH', "${sitePackageEnv};${process.env.PATH}",'User')`, [], { shell: 'powershell.exe' })
+      }
+    }
+
+    if (!await checkEnvironmentVariable('PATH', scriptEnv)) {
+      if (process.platform === "win32") {
+        await execa(`[Environment]::SetEnvironmentVariable('PATH', "${scriptEnv};${process.env.PATH}",'User')`, [], { shell: 'powershell.exe' })
+      }
+    }
+    
+  } catch (error) {
+    await log.error({
+      stderr: 'Impossible to register Pipenv environment variables',
+      commandPath: 'create birth',
+      value: error.toString()
+    })
+  }  
 }
