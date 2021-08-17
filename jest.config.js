213165feeb7d6ea5eb1896c295e8905aaa2541f2
Divlo <contact@divlo.fr>
2021-08-17 17:00:28 +0200

    test: add first End To End with Docker

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  rootDir: './src',
-  collectCoverage: true,
-  coverageDirectory: '../coverage/',
-  coverageReporters: ['text', 'cobertura']
-}
---
+++ src/e2e/docker.e2e.test.ts
@@ -0,0 +1,31 @@
+import waitOn from 'wait-on'
+
+import execa from 'execa'
+
+describe('Docker End To End (e2e)', () => {
+  it('should brings Leon to life and start the instance', (done) => {
+    const PORT = 1337
+    let startSubprocess: execa.ExecaChildProcess<string> | null = null
+    execa('leon', ['create', 'birth', '--docker', '--develop'])
+      .then(async ({ stdout }) => {
+        console.log(stdout)
+        startSubprocess = execa('leon', ['start', `--port=${PORT}`])
+        return await waitOn({
+          resources: [`http-get://localhost:${PORT}/`],
+          delay: 1000,
+          timeout: 30_000
+        })
+      })
+      .then(() => {
+        done()
+      })
+      .catch((error) => {
+        done(error)
+      })
+      .finally(() => {
+        if (startSubprocess != null) {
+          startSubprocess.kill('SIGINT')
+        }
+      })
+  })
+})
