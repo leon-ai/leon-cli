4e91d2667d8fc83a868068a768ca4aaacfc4ab57
Divlo <contact@divlo.fr>
2021-08-27 18:39:18 +0200

    refactor: services into classes

--- src/services/Pyenv.ts
+++ src/services/Pyenv.ts
@@ -6,25 +6,25 @@ import axios from 'axios'
 import execa from 'execa'
 import ora from 'ora'
 
-import { checkEnvironmentVariable } from './Requirements'
+import { requirements } from './Requirements'
 import { LogError } from '../utils/LogError'
 
-export class InstallPyenv {
-  static PYENV_WINDOWS_URL =
+class Pyenv {
+  static WINDOWS_URL =
     'https://codeload.github.com/pyenv-win/pyenv-win/zip/master'
 
   public async downloadWindowsZip(): Promise<AdmZip | undefined> {
     const downloadLoader = ora('Downloading Pyenv for Windows').start()
     try {
-      const body = await axios.get(InstallPyenv.PYENV_WINDOWS_URL, {
+      const body = await axios.get(Pyenv.WINDOWS_URL, {
         responseType: 'arraybuffer'
       })
       downloadLoader.succeed()
       return new AdmZip(body.data)
-    } catch (error) {
+    } catch (error: any) {
       downloadLoader.fail()
       throw new LogError({
-        message: `Could not download Pyenv Windows zip located at ${InstallPyenv.PYENV_WINDOWS_URL}`,
+        message: `Could not download Pyenv Windows zip located at ${Pyenv.WINDOWS_URL}`,
         logFileMessage: error.toString()
       })
     }
@@ -51,7 +51,7 @@ export class InstallPyenv {
         }
       })
       extractLoader.succeed()
-    } catch (error) {
+    } catch (error: any) {
       extractLoader.fail()
       throw new LogError({
         message: `Could not extract Pyenv Windows zip`,
@@ -60,7 +60,7 @@ export class InstallPyenv {
     }
   }
 
-  async getWindowsUserPath(): Promise<string> {
+  public async getWindowsUserPath(): Promise<string> {
     if (process.platform !== 'win32') {
       return ''
     }
@@ -72,14 +72,14 @@ export class InstallPyenv {
     return stdout
   }
 
-  async downloadPython(): Promise<void> {
+  public async downloadPython(): Promise<void> {
     const version = '3.9.6'
     const pythonLoader = ora(`Downloading python ${version}`).start()
     try {
       await execa(`pyenv install ${version} --quiet`)
       await execa(`pyenv global ${version}`)
       pythonLoader.succeed()
-    } catch (error) {
+    } catch (error: any) {
       pythonLoader.fail()
       throw new LogError({
         message: `Could not install python ${version}`,
@@ -88,12 +88,12 @@ export class InstallPyenv {
     }
   }
 
-  async rehash(): Promise<void> {
+  public async rehash(): Promise<void> {
     const rehashLoader = ora('Rehashing Pyenv commands').start()
     try {
       await execa('pyenv rehash')
       rehashLoader.succeed()
-    } catch (error) {
+    } catch (error: any) {
       rehashLoader.fail()
       throw new LogError({
         message: 'Could not rehash pyenv commands',
@@ -102,18 +102,12 @@ export class InstallPyenv {
     }
   }
 
-  async registerInPath(pyenvPath: string): Promise<void> {
+  public async registerInPathWindows(pyenvPath: string): Promise<void> {
     const varEnvLoader = ora('Registering environment variables').start()
     try {
-      if (!(await checkEnvironmentVariable('PYENV', 'pyenv-win'))) {
-        process.env.PYENV = `${pyenvPath}\\pyenv-win\\`
-        await execa(
-          `[Environment]::SetEnvironmentVariable('PYENV', "${pyenvPath}\\pyenv-win\\",'User')`,
-          [],
-          { shell: 'powershell.exe' }
-        )
-      }
-      if (!(await checkEnvironmentVariable('PYENV_HOME', 'pyenv-win'))) {
+      if (
+        !(await requirements.checkEnvironmentVariable('PYENV', 'pyenv-win'))
+      ) {
         process.env.PYENV = `${pyenvPath}\\pyenv-win\\`
         await execa(
           `[Environment]::SetEnvironmentVariable('PYENV', "${pyenvPath}\\pyenv-win\\",'User')`,
@@ -122,8 +116,27 @@ export class InstallPyenv {
         )
       }
       if (
-        !(await checkEnvironmentVariable('PATH', 'pyenv-win\\bin')) ||
-        !(await checkEnvironmentVariable('PATH', 'pyenv-win\\shims'))
+        !(await requirements.checkEnvironmentVariable(
+          'PYENV_HOME',
+          'pyenv-win'
+        ))
+      ) {
+        process.env.PYENV = `${pyenvPath}\\pyenv-win\\`
+        await execa(
+          `[Environment]::SetEnvironmentVariable('PYENV', "${pyenvPath}\\pyenv-win\\",'User')`,
+          [],
+          { shell: 'powershell.exe' }
+        )
+      }
+      if (
+        !(await requirements.checkEnvironmentVariable(
+          'PATH',
+          'pyenv-win\\bin'
+        )) ||
+        !(await requirements.checkEnvironmentVariable(
+          'PATH',
+          'pyenv-win\\shims'
+        ))
       ) {
         const userPath = await this.getWindowsUserPath()
         const extraPath = `${pyenvPath}\\pyenv-win\\bin;${pyenvPath}\\pyenv-win\\shims`
@@ -135,7 +148,7 @@ export class InstallPyenv {
         process.env.PATH = `${extraPath};${process.env.PATH ?? ''}`
       }
       varEnvLoader.succeed()
-    } catch (error) {
+    } catch (error: any) {
       varEnvLoader.fail()
       throw new LogError({
         message: 'Impossible to register Pyenv environment variables',
@@ -144,14 +157,23 @@ export class InstallPyenv {
     }
   }
 
-  public async onWindows(): Promise<void> {
+  public async installOnWindows(): Promise<void> {
     const destination = path.join(os.homedir(), '.pyenv')
     const zip = await this.downloadWindowsZip()
     if (zip != null) {
       await this.extractWindowsZip(zip, destination)
-      await this.registerInPath(destination)
+      await this.registerInPathWindows(destination)
     }
     await this.downloadPython()
     await this.rehash()
   }
+
+  public async install(): Promise<void> {
+    const isWindows = process.platform === 'win32'
+    if (isWindows) {
+      return await this.installOnWindows()
+    }
+  }
 }
+
+export const pyenv = new Pyenv()
--- src/services/__test__/Prompt.test.ts
+++ src/services/__test__/Prompt.test.ts
@@ -1,10 +1,9 @@
 import { acceptedAnswers } from '../Prompt'
 
-describe('services/Prompt - isValidAnswer', () => {
+describe('services/Prompt', () => {
   it('should accept yes, y, no, n as answers', () => {
     const acceptableAnswers = ['y', 'Y', 'yes', 'Yes', 'n', 'N', 'No', 'no']
     const wrongAnswers = ['Nope', 'Yay', 'Agreed', 'Anything you can imagine']
-
     for (const answer of acceptedAnswers) {
       expect(acceptableAnswers.includes(answer.toLowerCase())).toBe(true)
       expect(wrongAnswers.includes(answer.toLowerCase())).toBe(false)
