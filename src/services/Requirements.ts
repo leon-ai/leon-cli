c57dd1f0a2e049f385a9bf4e9e99395130585c10
Divlo <contact@divlo.fr>
2022-01-14 17:54:40 +0100

    fix: only use sudoExec outside test

--- src/services/Requirements.ts
+++ src/services/Requirements.ts
@@ -79,7 +79,7 @@ class Requirements {
     const scriptsPath = path.join(__dirname, '..', '..', 'scripts')
     const commandPath = path.join(scriptsPath, ...scriptCommand)
     try {
-      if (sudo) {
+      if (sudo && process.env.NODE_ENV !== 'test') {
         await sudoExec(commandPath)
       } else {
         await execa.command(commandPath)
@@ -127,7 +127,7 @@ class Requirements {
   }
 
   public async installPythonOnUnix(): Promise<void> {
-    // await this.installPackages()
+    await this.installPackages()
     await this.executeScript({
       scriptCommand: ['install_pyenv.sh'],
       loader: {
