66f4cc3aacd88d4a4e35d408ed7b57b040539c18
Divlo <contact@divlo.fr>
2022-01-30 20:23:09 +0100

    feat: notify users of new updates

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 import chalk from 'chalk'
 import { Cli } from 'clipanion'
+import updateNotifier from 'update-notifier'
 
 import { cli } from './cli.js'
+import { packageJSON } from './packageJSON.js'
 
 const [, , ...args] = process.argv
+
+const notifier = updateNotifier({
+  pkg: packageJSON
+})
+notifier.notify({ isGlobal: true })
 
 cli.runExit(args, Cli.defaultContext).catch(() => {
   console.error(chalk.red('Error occurred...'))
