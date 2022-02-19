dcc0fe1af58ad658c35762dae1a1e161e7ac2dac
Divlo <contact@divlo.fr>
2022-02-19 19:29:31 +0100

    feat(commands): add `leon kill`

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@ import { Builtins, Cli } from 'clipanion'
 import { CreateBirthCommand } from './commands/create/birth.js'
 import { CheckCommand } from './commands/check.js'
 import { HelpCommand } from './commands/help.js'
+import { KillCommand } from './commands/kill.js'
 import { StartCommand } from './commands/start.js'
 import { Leon } from './services/Leon.js'
 import { packageJSON } from './packageJSON.js'
@@ -17,4 +18,5 @@ cli.register(Builtins.VersionCommand)
 cli.register(CreateBirthCommand)
 cli.register(CheckCommand)
 cli.register(HelpCommand)
+cli.register(KillCommand)
 cli.register(StartCommand)
