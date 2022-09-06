29d5ceacf3630e213e98cccc5702e83a02ffe003
hj <jiehecd@gmail.com>
2022-09-06 22:31:44 +0800

    fix(scripts): argument `--yes` in `yum install` is unrecognized
    `--yes` is not a valid argument but `-y`. Replacing `--yes` with `-y` or using `yes` CLI can resolve this.
    
    Resolves: leon-ai/leon-cli/issues/195#issue-1363352738

--- scripts/dependencies/install_yum_packages.sh
+++ scripts/dependencies/install_yum_packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-yum install --yes gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel git bash curl
+yes | yum install gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel git bash curl
